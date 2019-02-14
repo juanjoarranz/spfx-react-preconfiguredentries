80400ea3407a553a6948728c5ba7f9759b27ba1a
Juanjo Arranz <juanjoarranzales@gmail.com>
2019-02-14 08:53:35 +0100

    Added Localization Labels for web part properties pane

--- src/webparts/gallery/loc/mystrings.d.ts
+++ src/webparts/gallery/loc/mystrings.d.ts
@@ -1,7 +1,14 @@
 declare interface IGalleryWebPartStrings {
-  PropertyPaneDescription: string;
-  BasicGroupName: string;
-  DescriptionFieldLabel: string;
+  PropertyPaneDescription           : string;
+  BasicGroupName                    : string;
+  ListNameFieldLabel                : string;
+  OrderFieldLabel                   : string;
+  OrderFieldChronologicalOptionLabel: string;
+  OrderFieldReversedOptionLabel     : string;
+  NumberOfItemsFieldLabel           : string;
+  StyleFieldLabel                   : string;
+  StyleFieldThumbnailsOptionLabel   : string;
+  StyleFieldListOptionLabel         : string;
 }
 
 declare module 'GalleryWebPartStrings' {
